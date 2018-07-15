711032db1b85136eb2465b261eac82e25b3f72e0
ERIC <ERIC@DESKTOP-HCMDCIU>
2018-07-15 11:33:20 +0800

    添加项目文件。

---
+++ ChangeFileMD5/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ChangeFileMD5
+{
+    class Class1
+    {
+        OracleConnection conn = new OracleConnection();
+    }
+}
---
+++ ChangeFileMD5/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ChangeFileMD5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvFilesInfo = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnLoadFiles = new System.Windows.Forms.Button();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilesInfo)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvFilesInfo
+            // 
+            this.dgvFilesInfo.AllowUserToAddRows = false;
+            this.dgvFilesInfo.AllowUserToDeleteRows = false;
+            this.dgvFilesInfo.AllowUserToResizeRows = false;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dgvFilesInfo.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvFilesInfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFilesInfo.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dgvFilesInfo.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgvFilesInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("宋体", 12F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvFilesInfo.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvFilesInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFilesInfo.Location = new System.Drawing.Point(3, 27);
+            this.dgvFilesInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvFilesInfo.Name = "dgvFilesInfo";
+            this.dgvFilesInfo.ReadOnly = true;
+            this.dgvFilesInfo.RowHeadersVisible = false;
+            this.dgvFilesInfo.RowTemplate.Height = 23;
+            this.dgvFilesInfo.Size = new System.Drawing.Size(1043, 521);
+            this.dgvFilesInfo.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Enabled = false;
+            this.btnStart.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(651, 20);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(147, 71);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始修改MD5";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLoadFiles
+            // 
+            this.btnLoadFiles.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnLoadFiles.ForeColor = System.Drawing.Color.Black;
+            this.btnLoadFiles.Location = new System.Drawing.Point(110, 20);
+            this.btnLoadFiles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLoadFiles.Name = "btnLoadFiles";
+            this.btnLoadFiles.Size = new System.Drawing.Size(127, 71);
+            this.btnLoadFiles.TabIndex = 2;
+            this.btnLoadFiles.Text = "载入文件";
+            this.btnLoadFiles.UseVisualStyleBackColor = true;
+            this.btnLoadFiles.Click += new System.EventHandler(this.btnLoadFiles_Click);
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTip.ForeColor = System.Drawing.Color.Red;
+            this.lblTip.Location = new System.Drawing.Point(289, 39);
+            this.lblTip.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(55, 29);
+            this.lblTip.TabIndex = 3;
+            this.lblTip.Text = "666";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 552);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1049, 42);
+            this.progressBar.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.dgvFilesInfo);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.Red;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1049, 552);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "注意：文件名中不能有空格";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnLoadFiles);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.lblTip);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 451);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1043, 97);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1049, 594);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar);
+            this.HelpButton = true;
+            this.LookAndFeel.SkinName = "Office 2010 Blue";
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MD5修改工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilesInfo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFilesInfo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnLoadFiles;
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
